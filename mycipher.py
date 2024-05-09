87312fe970887218e7494721377bbb9138a13726
rivcode <114775687+rivcode@users.noreply.github.com>
2024-05-09 11:51:22 -0400

    Code for Cipher Project

--- mycipher.py
+++ mycipher.py
@@ -1,1 +1,18 @@
 # CSCI213CeasarCipherProject
+def cipher(user_text, n):
+    ans = ""
+    for i in range(len(user_text)):
+        ch = user_text[i]
+        if ch == " ":
+            ans += " "
+        elif ch.isupper():
+            ans += chr((ord(ch) + n - 65) % 26 + 65)
+        else:
+            ans += chr((ord(ch) + n - 97) % 26 + 97)
+    return ans
+
+user_text = input("Enter text to encrypt: ")
+n = int(input("Enter the shift pattern (integer): "))
+print("Plain Text is: " + user_text)
+print("Shift pattern is: " + str(n))
+print("Cipher Text is: " + cipher(user_text, n))
